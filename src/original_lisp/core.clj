2c30bee3f59be6124d772b7f7e6de3e18e84a545
Kris Jenkins <krisajenkins@gmail.com>
2012-11-19 21:24:10 +0000

    Quote, atom, eq, car, cdr and cons work!

--- src/original_lisp/core.clj
+++ src/original_lisp/core.clj
@@ -1,6 +1,39 @@
 (ns original-lisp.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn atom? [x]
+  (not (seq? x)))
+
+(defn l-eval [expr env]
+  (cond
+    (atom? expr) (env expr)
+    (atom? (first expr)) (cond
+                           (= (first expr) 'quote) (first (rest expr))
+                           (= (first expr) 'atom) (atom? (l-eval (first (rest expr)) env))
+                           (= (first expr) 'eq) (= (l-eval (first (rest expr)) env)
+                                                   (l-eval (first (rest (rest expr))) env))
+                           (= (first expr) 'car) (first (l-eval (first (rest expr)) env))
+                           (= (first expr) 'cdr) (rest (l-eval (first (rest expr)) env))
+                           (= (first expr) 'cons) (cons (l-eval (first (rest expr)) env)
+                                                        (l-eval (first (rest (rest expr))) env)
+                                                        )
+                           )
+    )
+  )
+
+(l-eval '(quote (this that)) {'this 'five 'that 'ten})
+(l-eval '(car (this that)) {'this 'five 'that 'ten})
+
+(l-eval '(car ((quote this) (quote that))) {'this 'five 'that 'ten})
+(l-eval '(car (this that)) {'this 'five 'that 'ten})
+
+(l-eval '(car ((quote this) (quote that))) {'this 'five 'that 'ten})
+(first (first (rest '(car (this that)))))
+
+
+(defn l-evcon [c a]
+
+  )
+
+(defn l-evlis [m a]
+
+  )
---
+++ test/original_lisp/test/core.clj
@@ -0,0 +1,44 @@
+(ns original-lisp.test.core
+  (:use clojure.test
+        original-lisp.core))
+
+(deftest atom-test
+  (is (= (l-eval 'a {'a 5})
+         5)))
+
+(deftest quote-test
+  (is (= (l-eval '(quote thing) {})
+         'thing
+         )))
+
+(deftest atom-symbol-test
+  (is (= (l-eval '(atom thing) {'thing "THING"})
+         true))
+  (is (= (l-eval '(atom thing) {'thing '(1 2 3)})
+         false)))
+
+(deftest eq-test
+  (is (= (l-eval '(eq this that) {'this 5 'that 5})
+         true))
+  (is (= (l-eval '(eq this that) {'this 5 'that 10})
+         false)))
+
+(deftest car-test
+  (is (= (l-eval '(car (cons a (cons b ()))) {'a 1
+                                              'b 2})
+         1))
+  )
+
+(deftest cdr-test
+  (is (= (l-eval '(cdr (cons a (cons b ()))) {'a 1
+                                              'b 2})
+         '(2)))
+  )
+
+(deftest cons-test
+  (is (= (l-eval '(cons a ()) {'a 1})
+         '(1)))
+  (is (= (l-eval '(cons a (cons b ())) {'a 1
+                                        'b 2})
+         '(1 2)))
+  )
