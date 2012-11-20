e70ce93e80a0fdd95d7f4c67dd7ed357cc4441ce
Kris Jenkins <krisajenkins@gmail.com>
2012-11-20 13:28:22 +0000

    Eval function complete.
    We could consider renaming some functions, for clarity.

--- src/original_lisp/core.clj
+++ src/original_lisp/core.clj
@@ -8,21 +8,60 @@
   (or (not (seq? x))
       (empty? x)))
 
+(defn null? [x]
+  (and (seq? x)
+       (empty? x)))
+
+(defn pair
+  [xs ys]
+  {:pre [(= (count xs) (count ys))]}
+  (map list xs ys))
+
+; Some aliases.
+(def append concat)
+
+; NOTE - There seems to be a bug in Graham's paper. His version of assoc does not allow for x-not-found.
+(defn l-assoc
+  [x ys]
+  (cond
+    (= x (first (first ys))) (first (rest (first ys)))
+    (empty? ys) nil
+    :else (recur x (rest ys))))
+
 (defn l-eval [expr env]
   (cond
-    (atom? expr) (env expr)
+    (atom? expr) (l-assoc expr env)
+
     (atom? (first expr)) (cond
                            (= (first expr) 'quote) (first (rest expr))
-                           (= (first expr) 'atom) (atom? (l-eval (first (rest expr)) env))
-                           (= (first expr) 'eq) (= (l-eval (first (rest expr)) env)
-                                                   (l-eval (first (rest (rest expr))) env))
-                           (= (first expr) 'car) (first (l-eval (first (rest expr)) env))
-                           (= (first expr) 'cdr) (rest (l-eval (first (rest expr)) env))
-                           (= (first expr) 'cons) (cons (l-eval (first (rest expr)) env)
-                                                        (l-eval (first (rest (rest expr))) env)))))
+                           (= (first expr) 'atom)  (atom? (l-eval (first (rest expr)) env))
+                           (= (first expr) 'eq)    (= (l-eval (first (rest expr)) env)
+                                                      (l-eval (first (rest (rest expr))) env))
+                           (= (first expr) 'car)   (first (l-eval (first (rest expr)) env))
+                           (= (first expr) 'cdr)   (rest (l-eval (first (rest expr)) env))
+                           (= (first expr) 'cons)  (cons (l-eval (first (rest expr)) env)
+                                                         (l-eval (first (rest (rest expr))) env))
+                           (= (first expr) 'cond)  (l-evcon (rest expr) env)
+                           :else                   (l-eval (cons (l-assoc (first expr) env) (rest expr))
+                                                           env))
 
-(defn l-evcon [c a]
-  )
+    (= (first (first expr)) 'label)  (l-eval (cons (first (rest (rest (first expr))))
+                                                   (rest expr))
+                                             (cons (list (first (rest (first expr)))
+                                                         (first expr))
+                                                   env))
 
-(defn l-evlis [m a]
-  )
+    (= (first (first expr)) 'lambda) (l-eval (first (rest (rest (first expr))))
+                                             (append (pair (first (rest (first expr))) (l-evlis (rest expr) env))
+                                                     env))))
+
+(defn l-evcon [condition env]
+  (cond
+    (l-eval (first (first condition)) env) (l-eval (first (rest (first condition))) env)
+    :else (recur (rest condition) env)))
+
+(defn l-evlis [m env]
+  (cond
+    (null? m) '()
+    :else (cons (l-eval (first m) env)
+                (l-evlis (rest m) env))))
--- test/original_lisp/test/core.clj
+++ test/original_lisp/test/core.clj
@@ -7,43 +7,110 @@
   (is (atom? '()))
   (is (not (atom? '(1 2 3)))))
 
-(deftest atom-test
-  (is (= (l-eval 'a {'a 5})
-         5)))
+(deftest null?-test
+  (is (not (null? 'a)))
+  (is (null? '()))
+  (is (not (null? '(1 2 3)))))
 
-(deftest quote-test
-  (is (= (l-eval '(quote thing) {})
-         'thing
+(deftest pair-test
+  (is (= (pair '(a b c) '(x y z))
+         '((a x) (b y) (c z))
          )))
 
+(deftest append-test
+  (is (= (append '(a b) '(x y))
+         '(a b x y))))
+
+(deftest l-assoc-test
+  (is (= (l-assoc 'x '((x a) (y b)))
+         'a))
+  (is (= (l-assoc 'x '((x new) (x a) (y b)))
+         'new))
+  (is (= (l-assoc 'y '((x new) (x a) (y b)))
+         'b)))
+
+(deftest atom-test
+  (is (= (l-eval 'a '((a 5) (b 10)))
+         5))
+  (is (= (l-eval 'b '((a 5) (b 10)))
+         10)))
+
+(deftest quote-test
+  (is (= (l-eval '(quote thing) '())
+         'thing)))
+
 (deftest atom-symbol-test
-  (is (= (l-eval '(atom thing) {'thing "THING"})
+  (is (= (l-eval '(atom thing) '((thing "THING")))
          true))
-  (is (= (l-eval '(atom thing) {'thing '(1 2 3)})
+  (is (= (l-eval '(atom thing) '((thing '(1 2 3))))
          false)))
 
 (deftest eq-test
-  (is (= (l-eval '(eq this that) {'this 5 'that 5})
+  (is (= (l-eval '(eq this that) '((this 5) (that 5)))
          true))
-  (is (= (l-eval '(eq this that) {'this 5 'that 10})
+  (is (= (l-eval '(eq this that) '((this 5) (that 10)))
          false)))
 
 (deftest car-test
-  (is (= (l-eval '(car (cons a (cons b ()))) {'a 1
-                                              'b 2})
-         1))
-  )
+  (is (= (l-eval '(cons a (cons b '())) '((a 1) (b 2)))
+         '(1 2)))
+  (is (= (l-eval '(car (cons a (cons b '()))) '((a 1) (b 2)))
+         1)))
 
 (deftest cdr-test
-  (is (= (l-eval '(cdr (cons a (cons b ()))) {'a 1
-                                              'b 2})
-         '(2)))
-  )
+  (is (= (l-eval '(cdr (cons a (cons b '()))) '((a 1) (b 2)))
+         '(2))))
 
 (deftest cons-test
-  (is (= (l-eval '(cons a ()) {'a 1})
+  (is (= (l-eval '(cons a '()) '((a 1)))
          '(1)))
-  (is (= (l-eval '(cons a (cons b ())) {'a 1
-                                        'b 2})
-         '(1 2)))
-  )
+  (is (= (l-eval '(cons a (cons b '())) '((a 1) (b 2)))
+         '(1 2))))
+
+(deftest cond-test
+  (is (= (l-eval '(cond (a b) (c d)) '((a 1) (b 2) (c 3) (d 4)))
+         2))
+  (is (= (l-eval '(cond (a b) (c d)) '((b 2) (c 3) (d 4)))
+         4)))
+
+(deftest substitute-head-test
+  (is (= (l-eval '(woo thing) '((woo quote)))
+         'thing)))
+
+(deftest label-test
+  (is (= (l-eval '((label firstatom (lambda (x)
+                                            (cond ((atom x) x)
+                                                  ('t (firstatom (car x))))))
+                     y)
+                 '((y ((a b) (c d)))))
+         'a)))
+
+(deftest lambda-test
+  (is (= (l-eval '(f '(b c))
+                 '((f (lambda (x) (cons 'a x)))))
+         '(a b c))))
+
+(deftest paul-graham-examples
+  (is (= (l-eval 'x '((x a) (y b)))
+         'a))
+
+  (is (l-eval '(eq a a) '()))
+
+  (is (= (l-eval '(cons x '(b c))
+                 '((x a) (y b)))
+         '(a b c)))
+
+  (is (= (l-eval '(cond ((atom x) 'atom)
+                        ('t 'list))
+                 '((x (a b))))
+         'list))
+
+  (is (= (l-eval '(f '(b c))
+                 '((f (lambda (x) (cons 'a x)))))
+         '(a b c)))
+
+  (is (= (l-eval '((lambda (x y) (cons x (cdr y)))
+                     'a
+                     '(b c d))
+                 '())
+        '(a c d))))
